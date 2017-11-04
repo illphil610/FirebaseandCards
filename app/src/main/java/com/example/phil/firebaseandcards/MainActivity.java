18ea3546f5f43235ad82891dcf34984827634ea3
Philip <pcappelli901@gmail.com>
2017-11-04 16:36:04 -0400

    Added log in/out auth capability

--- app/src/main/java/com/example/phil/firebaseandcards/MainActivity.java
+++ app/src/main/java/com/example/phil/firebaseandcards/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.phil.firebaseandcards;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -16,6 +17,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.squareup.picasso.Picasso;
@@ -24,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
 
     private RecyclerView mRecyclerView;
     private DatabaseReference mDatabase;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,12 +40,25 @@ public class MainActivity extends AppCompatActivity {
         mRecyclerView.setHasFixedSize(true);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         mDatabase = FirebaseDatabase.getInstance().getReference().child("CardApp");
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if (firebaseAuth.getCurrentUser() == null) {
+                    Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+                    loginIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(loginIntent);
+                }
+            }
+        };
     }
 
     @Override
     protected void onStart() {
         super.onStart();
 
+        mAuth.addAuthStateListener(mAuthListener);
         FirebaseRecyclerAdapter <EventReport, EventViewHolder> FBRA = new FirebaseRecyclerAdapter<EventReport, EventViewHolder>(
                 EventReport.class, R.layout.card_row, EventViewHolder.class, mDatabase) {
             @Override
@@ -77,6 +94,10 @@ public class MainActivity extends AppCompatActivity {
         if (id == R.id.add_icon) {
             Intent intent = new Intent(MainActivity.this, PostActivity.class);
             startActivity(intent);
+        }
+
+        if (id == R.id.log_out) {
+            mAuth.signOut();
         }
         return super.onOptionsItemSelected(item);
     }
