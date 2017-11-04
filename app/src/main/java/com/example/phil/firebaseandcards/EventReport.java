1845a67f212f06ed2bcd8f183b058c6deedf473e
Philip <pcappelli901@gmail.com>
2017-11-04 14:09:20 -0400

    Implemented a RecyclerView which uses a CardView.  I populate the cards with data from Firebase.  Currently pulling in values for 2 TextViews and 1 ImageView.  Next going to work on a bug that causes the app to crash when the user creates an event, and navigates back to the feed actvitiy.

---
+++ app/src/main/java/com/example/phil/firebaseandcards/EventReport.java
@@ -0,0 +1,44 @@
+package com.example.phil.firebaseandcards;
+
+/**
+ * Created by phil on 11/4/17.
+ */
+
+public class EventReport {
+
+    private String title, description, image;
+
+    public EventReport() {
+
+    }
+
+    public EventReport(String title, String description, String image) {
+        this.title = title;
+        this.description = description;
+        this.image = image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setmDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
--- app/src/main/java/com/example/phil/firebaseandcards/MainActivity.java
+++ app/src/main/java/com/example/phil/firebaseandcards/MainActivity.java
@@ -1,15 +1,29 @@
 package com.example.phil.firebaseandcards;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +32,26 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        mRecyclerView = (RecyclerView) findViewById(R.id.card_list);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("CardApp");
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseRecyclerAdapter <EventReport, EventViewHolder> FBRA = new FirebaseRecyclerAdapter<EventReport, EventViewHolder>(
+                EventReport.class, R.layout.card_row, EventViewHolder.class, mDatabase) {
+            @Override
+            protected void populateViewHolder(EventViewHolder viewHolder, EventReport model, int position) {
+                viewHolder.setTitle(model.getTitle());
+                viewHolder.setDesc(model.getDescription());
+                viewHolder.setImage(getApplicationContext(), model.getImage());
+            }
+        };
+        mRecyclerView.setAdapter(FBRA);
     }
 
     @Override
@@ -27,6 +60,7 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -44,8 +78,29 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(MainActivity.this, PostActivity.class);
             startActivity(intent);
         }
-
-
         return super.onOptionsItemSelected(item);
+    }
+
+    public static class EventViewHolder extends RecyclerView.ViewHolder {
+
+        public EventViewHolder(View itemView) {
+            super(itemView);
+            View mView = itemView;
+        }
+
+        public void setTitle(String title) {
+            TextView event_title = itemView.findViewById(R.id.report_title);
+            event_title.setText(title);
+        }
+
+        public void setDesc(String desc) {
+            TextView event_desc = itemView.findViewById(R.id.report_desc);
+            event_desc.setText(desc);
+        }
+
+        public void setImage(Context context, String image) {
+            ImageView event_image = itemView.findViewById(R.id.report_card);
+            Picasso.with(context).load(image).into(event_image);
+        }
     }
 }
